956785096f69f3bb16480f2144216cc6fa9ce9ac
banky <banklan2010@gmail.com>
2022-07-08 08:51:06 +0100

    deploy20

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-///// to sove cors blocking issues
-header('Access-Control-Allow-Origin: *');
-header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
